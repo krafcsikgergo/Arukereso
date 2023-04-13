bf75174736580e193ee8192780584a1de8eefa60
Gigachad22 <104797450+Gigachad22@users.noreply.github.com>
2023-04-13 13:39:00 +0200

    routing

--- app.js
+++ app.js
@@ -6,15 +6,74 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/alma", (req, res) => {
-  const data = { a: 2 };
-  res.render("alma.ejs", { data });
-});
+// '/' route
+app.get("/", renderMW(objRepo, "index"));
+app.post("/", getAruhazakBySearchMW(objRepo), renderMW(objRepo, "kereses"));
+
+// '/aruhazak' route
+app.get(
+  "/aruhazak",
+  getAruhazakBySearchMW(objRepo),
+  renderMW(objRepo, "aruhazak")
+);
+app.post(
+  "/aruhazak",
+  getAruhazakBySearchMW(objRepo),
+  redirectMW(objRepo, "kereses")
+);
+
+app.post(
+  "aruhazak/delete/id/:aruhazId",
+  deleteAruhazMW(objRepo),
+  redirectMW(objRepo, "aruhazak")
+);
+
+app.get(
+  "/aruhazak/id/:aruhazId",
+  getAruhazByIdDBMW(objRepo),
+  getProductsFromAruhazMW(objRepo),
+  renderMW(objRepo, "aruhazTermekek")
+);
+
+app.post(
+  "/aruhazak/id/:aruhazId/deleteProduct/:productId",
+  deleteProductMW(objRepo),
+  redirectMW(objRepo, "aruhazTermekek")
+);
+
+app.get("/aruhazak/id/:aruhazId/newProduct", renderMW(objRepo, "addProduct"));
+
+app.post(
+  "/aruhazak/id/:aruhazId/newProduct",
+  newProductToDBMW(objRepo),
+  redirectMW(objRepo, "aruhazTermekek")
+);
+
+app.get(
+  "/aruhazak/id/:aruhazId/modifyProduct/:productId",
+  getProductByIdMW(objRepo),
+  renderMW(objRepo, "modifyProduct")
+);
+app.post(
+  "/aruhazak/id/:aruhazId/modifyProduct/:productId",
+  modifyProductMW(objRepo),
+  redirectMW(objRepo, "aruhazTermekek")
+);
+
+app.get(
+  "/aruhazak/edit/id/:aruhazId",
+  getAruhazByIdDBMW(objRepo),
+  renderMW(objRepo, "szerkAruhaz.html")
+);
+app.post("/aruhazak/edit/id/:aruhazId");
+
+
+
+
+
+
+
 
 app.listen(PORT, (error) => {
-  if (!error)
-    console.log(
-      "Server is Successfully Running, and App is listening on port " + PORT
-    );
-  else console.log("Error occurred, server can't start", error);
+  console.log("Server is running on port 3000");
 });
