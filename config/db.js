1d7eff21bf230c25e2e9dc87097a6f9a2c9a2c51
Gigachad22 <104797450+Gigachad22@users.noreply.github.com>
2023-05-12 15:19:10 +0200

    containerizing app

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const url = 'mongodb://127.0.0.1:27017/PAI2R6';
+require('dotenv').config();
+const url = process.env.MONGODB_URL;
 mongoose.connect(url, {
   useNewUrlParser: true,
 });
